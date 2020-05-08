16a7812302d19383c40ad398234fece3357840f7
lucascprazeres <olucascostadp@gmail.com>
2020-05-07 23:17:25 -0300

    eslint + editorconfig

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <h1>
+      Hello, World!
+    </h1>
+  );
 }
 
 export default App;
